0924c1eae3d7704ab2cb4d62eb8323378123310c
Pearu Pung <pungpearu@gmail.com>
2018-09-12 13:11:02 +0300

    Added pointer functionality to print_map

--- Minesweeper/test_example.py
+++ Minesweeper/test_example.py
@@ -1,19 +1,32 @@
 
 
-def print_map(game_field):
-    row_element_count = len(game_field[0])
-    row_index_len = len(str(row_element_count))
-    row_length = 2 * row_element_count + row_index_len + 2
+def print_map(game_field, pointer_coords=(1,1)):
 
-    column_indices = map(str, range(row_element_count))
-    #print(f"   {' '.join(column_indices)}|")
-    print(" " * row_length)
+    row_count = len(game_field)
+    col_count = len(game_field[0])
+    pointer_row, pointer_col = pointer_coords
+
+
+    if not pointer_row < row_count:
+        raise Exception(f"Pointer out of bounds: field has {row_count} rows, pointer is at index {pointer_row}.")
+    if not pointer_col < col_count:
+        raise Exception(f"Pointer out of bounds: field has {col_count} columns, pointer is at index {pointer_col}.")
 
     for row_index, row in enumerate(game_field):
 
-        row_elements = map(str, game_field[row_index])
-        print(f"{' '.join(row_elements)}")
-        print(" " * row_length)
+        row_elements = list(map(str, game_field[row_index]))
+
+        if row_index == pointer_row and pointer_col != 0:
+            print(f" {'  '.join(row_elements[:pointer_col])}" + \
+                  f" <{row_elements[pointer_col]}> " + \
+                  '  '.join(row_elements[pointer_col + 1:]))
+
+        elif row_index == pointer_row and pointer_col == 0:
+            print(f"<{row_elements[0]}> {'  '.join(row_elements[1:])}")
+
+        else:
+            print(f" {'  '.join(row_elements)}")
+
 
 if __name__ == "__main__":
 
@@ -24,4 +37,12 @@ if __name__ == "__main__":
               [4,5,6,7,8,9]]
 
 
-    print_map(field1)
+    print_map(field1, (0, 0))
+    print()
+    print_map(field1, (0, 1))
+    print()
+    print_map(field1, (3, 3))
+    print()
+    # Row/columns error
+    # print_map(field1, (5, 3))
+    # print_map(field1, (4, 6))
