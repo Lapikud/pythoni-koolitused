3edf78416bb91fa35b4f7ec16ed103abc700a49a
Oskar <26824310+OskaruSenpai@users.noreply.github.com>
2018-11-27 17:30:51 +0200

    Removed even more clutter + minor tweaks
    Made all sides hit at least 200 when they are facing straight up.
    Removed misleading comments
    Removed unused variables

--- LEDcube/ledid_polevad/ledid_polevad.ino
+++ LEDcube/ledid_polevad/ledid_polevad.ino
@@ -3,7 +3,6 @@
 // TODO: Round/Stabalize the accelerator reads, to prevent shaking messing up lighting so much
 
 const int MPU = 0x68;
-double Tmp, GyX, GyY, GyZ; // Set-up  read data values
 double AcReads[3]; // To be able to use for-loops to set LED-values
 
 const int pins[] = {3, 5, 6, 9, 10, 11}; // Pins in use
@@ -24,52 +23,24 @@ void loop() {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  //Wire.requestFrom(MPU, 14, true);
   Wire.requestFrom(MPU, 6, true);
 
-  double AcXoff, AcYoff, AcZoff; // Fix readings
-
-  // Acceleration data correction
-  AcXoff = -950;
-  AcYoff = -300;
-  AcZoff = 0;
-
   // Read Accelerator data and set it up for calculations
-  AcReads[1] = ((Wire.read() << 8 | Wire.read()) + AcXoff); // AcX
-  AcReads[2] = ((Wire.read() << 8 | Wire.read()) + AcYoff); // AcY
-  AcReads[0] = ((Wire.read() << 8 | Wire.read()) + AcZoff); // AcZ
+  AcReads[1] = ((Wire.read() << 8 | Wire.read())); // AcX
+  AcReads[2] = ((Wire.read() << 8 | Wire.read())); // AcY
+  AcReads[0] = ((Wire.read() << 8 | Wire.read())); // AcZ
 
   // Send data to LED's
   for (int i = 0; i < sizeof(pins) / sizeof(int); i++) {
-    int temp = ((1 - 2 * (i % 2)) * AcReads[i/2]+20000)/1000;
-    analogWrite(pins[i], (temp*temp*temp) / 270);
+    double temp = ((1 - 2 * (i % 2)) * AcReads[i / 2] + 20000) / 1000;
+    analogWrite(pins[i], temp * temp * temp / 270 * 1.5);
   }
-
-  /*
-    Explaining the for-loop above:
-
-    analogWrite(pins[0], (((AcReads[0] + 20000) / 157)));
-    analogWrite(pins[1], (((-AcReads[0] + 20000) / 157)));
-    analogWrite(pins[2], (((AcReads[1] + 20000) / 157)));
-    analogWrite(pins[3], (((-AcReads[1] + 20000) / 157)));
-    analogWrite(pins[4], (((AcReads[2] + 20000) / 157)));
-    analogWrite(pins[5], (((-AcReads[2] + 20000) / 157)));
-  */
 
   // For serial-plotter data -> Ctrl + Shift + L
   for (int i = 0; i < sizeof(pins) / sizeof(int); i++) {
     Serial.print(" ");
-    int temp = ((1 - 2 * (i % 2)) * AcReads[i/2]+20000)/1000;
-    Serial.print((temp*temp*temp) / 270);
+    double temp = ((1 - 2 * (i % 2)) * AcReads[i / 2] + 20000) / 1000;
+    Serial.print(temp * temp * temp / 270 * 1.5);
   }
   Serial.println();
-  /*
-  // Read temperature data so accelerator data would be read from the right place
-  Tmp = (Wire.read() << 8 | Wire.read());
-
-  // Read gyro data so accelerator data would be read from the right place
-  GyX = (Wire.read() << 8 | Wire.read());
-  GyY = (Wire.read() << 8 | Wire.read());
-  GyZ = (Wire.read() << 8 | Wire.read());
-  */
 }
